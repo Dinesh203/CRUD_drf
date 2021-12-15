cf12cdb45001e5173adad1c2c52c9d61f83e6124
Dinesh203 <dineshparihar203@gmail.com>
2021-12-16 02:42:04 +0530

    modelviewsets_done

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,14 +1,21 @@
 
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from myapp.views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register(r'viewses', ApiViewSets)
+
 
 
 urlpatterns = [
     path("", HelloView.as_view(), name='hello'),
-    path("modelviews/", ApiViewSets.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update',
-                                            'delete': 'destroy'}), name='model_views'),
+    path("viewsets/", include(router.urls)),
+    # path("modelviews/", ApiViewSets.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update',
+    #                                         'delete': 'destroy'}), name='model_views'),
     path("userinfo/<int:pk>", UserDetail.as_view())
 
 ]
-urlpatterns = format_suffix_patterns(urlpatterns)
+
